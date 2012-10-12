a72f227d1e375caeeddd0e4b77e66cbb5786afa5
Anatoliy <nuclon@gmail.com>
2012-10-12 07:09:47 +0300

    .hgignore + Qless::Jobs

--- lib/Qless/Jobs.pm
+++ lib/Qless/Jobs.pm
@@ -2,15 +2,40 @@ package Qless::Jobs;
 use strict; use warnings;
 sub new {
 	my $class = shift;
-	my ($client) = @_;
+	my ($name, $client) = @_;
 
 	$class = ref $class if ref $class;
 	my $self = bless {}, $class;
 
+	$self->{'name'}   = $name;
 	$self->{'client'} = $client;
 
 	$self;
 }
 
+sub running {
+	my ($self, $offset, $count) = @_;
+	return $self->{'client'}->_jobs([], 'running', time, $self->{'name'}, $offset||0, $count||25);
+}
+
+sub stalled {
+	my ($self, $offset, $count) = @_;
+	return $self->{'client'}->_jobs([], 'stalled', time, $self->{'name'}, $offset||0, $count||25);
+}
+
+sub scheduled {
+	my ($self, $offset, $count) = @_;
+	return $self->{'client'}->_jobs([], 'scheduled', time, $self->{'name'}, $offset||0, $count||25);
+}
+
+sub depends {
+	my ($self, $offset, $count) = @_;
+	return $self->{'client'}->_jobs([], 'depends', time, $self->{'name'}, $offset||0, $count||25);
+}
+
+sub recurring {
+	my ($self, $offset, $count) = @_;
+	return $self->{'client'}->_jobs([], 'recurring', time, $self->{'name'}, $offset||0, $count||25);
+}
 
 1;
